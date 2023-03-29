6d1ceacca87f71a3b18fe96d7cce5251b8096922
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-29 16:24:28 +0300

    update

--- src/components/FavoriteStops.tsx
+++ src/components/FavoriteStops.tsx
@@ -1,47 +1,55 @@
 import { Box, Divider, FlatList, HStack, Text } from "native-base";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+type a = {
+    data: Stops[]
+}
+type Stops = {
+    "gtfsId": string,
+    "name": string
+}
 
 export default function FavoriteStops() {
-    const [favoriteStops, setFavoriteStops] = useState([
-        {
-            "gtfsId": "HSL:6150219",
-            "name": "Louhosmäki",
-            "lat": 60.202282,
-            "lon": 24.358088,
-            "zoneId": "D"
-        },
-        {
-            "gtfsId": "HSL:6150218",
-            "name": "Solvik",
-            "lat": 60.201517,
-            "lon": 24.375928,
-            "zoneId": "D"
-        },
-        {
-            "gtfsId": "HSL:6150217",
-            "name": "Evitskog",
-            "lat": 60.20141,
-            "lon": 24.342,
-            "zoneId": "D"
-        },
-        {
-            "gtfsId": "HSL:6150216",
-            "name": "Evitskogin VPK",
-            "lat": 60.20103,
-            "lon": 24.33853,
-            "zoneId": "D"
-        },]);
+    const [favoriteStops, setFavoriteStops] = useState<Stops[]>([]);
+
+    useEffect(() => {
+        getStops();
+    }, []);
+
+    const getStops = async () => {
+        const body = `{
+                        stops(name: "Tuomarila", maxResults: 5) {
+                            gtfsId
+                            name
+                        }
+                    }`
+        const config = {
+            headers: { "Content-Type": "application/graphql" },
+            method: "POST",
+            body: body
+        }
+
+        const response = await fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', config);
+        if (!response.ok) {
+            console.error(response.status);
+        }
+        const responseData = await response.json();
+        setFavoriteStops(responseData.data.stops)
+
+    }
+
 
 
     return (
-        <Box variant='homeScreenBox'  >
+        <Box variant='homeScreenBox'>
             <FlatList
                 data={favoriteStops}
                 renderItem={({ item }) =>
                     <Box p='2' >
                         <Text variant='homeScreenBoxText'>{item.name}</Text>
                         <Divider bg='muted.200' marginTop='2' />
-                    </Box>} />
+                    </Box>
+                } />
         </Box>
     )
 }
