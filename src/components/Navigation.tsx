c2a58f7d1992c59afb9634c0a118a251d638e85b
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-04-04 16:19:04 +0300

    Added icons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,13 +2,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import MapScreen from '../screens/MapScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+
+    const screenOptions = ({ route }: any) => ({
+        tabBarIcon: ({ focused, color, size }: any) => {
+            let iconName: string | any;
+
+            if (route.name === 'Etusivu') {
+                iconName = 'home';
+            } else if (route.name === 'Kartta') {
+                iconName = 'map';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+        }
+    });
+
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator screenOptions={screenOptions} >
                 <Tab.Screen name="Etusivu" component={HomeScreen} />
                 <Tab.Screen name='Kartta' component={MapScreen} />
             </Tab.Navigator>
