de72a5c59164daadb0110773610b3dc758dc9fe7
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-04-05 13:39:48 +0300

    Added functions for counting time and formatting time

---
+++ src/utils/TimeCalculations.tsx
@@ -0,0 +1,4 @@
+export const countHours = (timeInSeconds: number): number => Math.floor(timeInSeconds / 3_600);
+
+export const countMinutes = (timeInSeconds: number): number => (timeInSeconds % 3_600) / 60;
+
