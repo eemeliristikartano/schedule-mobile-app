63b23211d2bdce039d6d4967184748fd2de724ac
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-05-07 16:35:52 +0300

    Added sign out feature

--- src/navigation/UserNavigation.tsx
+++ src/navigation/UserNavigation.tsx
@@ -4,10 +4,13 @@ import HomeScreen from '../screens/HomeScreen';
 import MapScreen from '../screens/MapScreen';
 import { Ionicons } from '@expo/vector-icons';
 import StopSearchScreen from '../screens/StopSearchScreen';
+import { Icon, IconButton } from 'native-base';
+import { getAuth, signOut } from 'firebase/auth';
 
 const Tab = createBottomTabNavigator();
 
 export default function UserNavigation() {
+    const auth = getAuth();
 
     const screenOptions = ({ route }: any) => ({
         tabBarIcon: ({ focused, color, size }: any) => {
@@ -29,7 +32,19 @@ export default function UserNavigation() {
     return (
         <NavigationContainer>
             <Tab.Navigator screenOptions={screenOptions} >
-                <Tab.Screen name="Etusivu" component={HomeScreen} />
+                <Tab.Screen
+                    name="Etusivu"
+                    component={HomeScreen}
+                    options={{
+                        headerRight: () => (
+                            <IconButton
+                                size='lg'
+                                icon={<Icon as={Ionicons} name='log-out-outline' />}
+                                onPress={() => signOut(auth)}
+                            />
+                        )
+                    }}
+                />
                 <Tab.Screen name='Lähipysäkit' component={MapScreen} />
                 <Tab.Screen name='Pysäkkihaku' component={StopSearchScreen} />
             </Tab.Navigator>
